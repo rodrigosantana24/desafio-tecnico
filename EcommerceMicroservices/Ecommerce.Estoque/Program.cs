d330d8340d209015bd82dc94f707ceed1629965f
Rodrigo Santana <rodrigosantana.dev@gmail.com>
2025-11-21 16:18:51 -0300

    Update Program.cs

--- EcommerceMicroservices/Ecommerce.Estoque/Program.cs
+++ EcommerceMicroservices/Ecommerce.Estoque/Program.cs
@@ -1,8 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddDbContext<EstoqueContext>(opt => opt.UseInMemoryDatabase("EstoqueDb")); 
+builder.Services.AddDbContext<EstoqueContext>(opt => opt.UseSqlite("Data Source=estoque.db")); 
 builder.Services.AddHostedService<RabbitMqConsumer>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
